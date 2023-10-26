c2273c1c9734e021a569f751f3072a896b009cf9
charlesalo <charlesalo@gmail.com>
2023-10-26 15:01:01 +0800

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ChecklistRtlIcon from '@mui/icons-material/ChecklistRtl';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        Keeper
+        <ChecklistRtlIcon />
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
