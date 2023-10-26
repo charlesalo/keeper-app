3397dd85c05f2c3e302444fd1a1e7a5f3c7f33b2
charlesalo <charlesalo@gmail.com>
2023-10-26 18:52:06 +0800

    fix ReactDom deprecation warning

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
